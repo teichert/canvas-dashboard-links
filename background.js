4ded7208e4e4d41005b86aa7574ab5d3e3024de7
Adam Teichert <adam.teichert@snow.edu>
2025-11-05 20:03:16 -0700

    (unverified wip) opens attendance relative link and prepends title with original title

--- background.js
+++ background.js
@@ -19,8 +19,46 @@ chrome.action.onClicked.addListener(() => {
                     if (urls.length) {
                         clearInterval(interval);
                             urls.forEach(url => {
-                                const newUrl = url;
-                                chrome.tabs.create({ windowId, url: newUrl });
+                                chrome.tabs.create({ windowId, url: url }, (tab) => {
+                                    let courseTitle = '';
+                                    let loadCount = 0;
+                                    
+                                    // Wait for the tab to load to get the course name
+                                    chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+                                        if (tabId === tab.id && info.status === 'complete') {
+                                            loadCount++;
+                                            
+                                            if (loadCount === 1) {
+                                                // First load - got the course name
+                                                chrome.tabs.get(tabId, (updatedTab) => {
+                                                    courseTitle = updatedTab.title;
+                                                    // Navigate to the relative link
+                                                    chrome.scripting.executeScript({
+                                                        target: { tabId: tabId },
+                                                        func: () => {
+                                                            window.location.href = window.location.href + "/external_tools/3809";
+                                                        }
+                                                    });
+                                                });
+                                            } else if (loadCount === 2) {
+                                                // Second load - got the attendance page
+                                                chrome.tabs.onUpdated.removeListener(listener);
+                                                chrome.tabs.get(tabId, (updatedTab) => {
+                                                    const attendanceTitle = updatedTab.title;
+                                                    // Combine both titles
+                                                    const combinedTitle = `${courseTitle} - ${attendanceTitle}`;
+                                                    chrome.scripting.executeScript({
+                                                        target: { tabId: tabId },
+                                                        func: (newTitle) => {
+                                                            document.title = newTitle;
+                                                        },
+                                                        args: [combinedTitle]
+                                                    });
+                                                });
+                                            }
+                                        }
+                                    });
+                                });
                             });
                     }
                 });
